3e6b08e64d988c60e4ddf75223964ae387750e2d
Eric Bakan <eric@ebakan.com>
2012-01-15 22:08:26 -0800

    Added ConcurrentCommand implementation

---
+++ ConcurrentCommand.cpp
@@ -0,0 +1,43 @@
+#include "ConcurrentCommand.h"
+#include <stdarg.h>
+
+ConcurrentCommand::ConcurrentCommand(int numCommands, ...) {
+  va_list(vl);
+  va_start(vl,numCommands);
+  for(int i = 0; i < numCommands; i++) {
+    AutoCommand* command = va_arg(vl,AutoCommand*);
+    commandPair cPair(command,false);
+    commands_.push_back(cPair);
+  }
+  va_end(vl);
+}
+
+ConcurrentCommand::~ConcurrentCommand() {
+}
+
+void ConcurrentCommand::Initialize() {
+  std::vector<commandPair>::iterator it;
+  for(it = commands_.begin(); it < commands_.end(); it++) {
+    commandPair cPair = *it;
+    cPair.first->Initialize();
+  }
+
+}
+
+bool ConcurrentCommand::Run() {
+  std::vector<commandPair>::iterator it;
+  bool done=true;
+  for(it = commands_.begin(); it < commands_.end(); it++) {
+    commandPair cPair = *it;
+    // if the command isn't done yet, update its doneness status
+    if(!cPair.second) {
+      cPair.second=cPair.first->Run();
+    }
+    // if the command isn't done, done will be false
+    done&=cPair.second;
+  }
+
+  return done;
+
+}
+
