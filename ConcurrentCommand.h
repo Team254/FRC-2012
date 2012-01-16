02e797b0c0b400ad1401a8ca0abebebc3df49fd7
Eric Bakan <eric@ebakan.com>
2012-01-15 23:54:06 -0800

    Commented ConcurrentCommand.h, added include guards

--- ConcurrentCommand.h
+++ ConcurrentCommand.h
@@ -3,17 +3,48 @@
 
 #include "AutoCommand.h"
 
+/**
+ * @author Eric Bakan
+ *
+ * Meta-command used for running multiple commands concurrently
+ */
+
+#ifndef CONCURRENT_COMMAND_H_
+#define CONCURRENT_COMMAND_H_
+
 class ConcurrentCommand : public AutoCommand {
  public:
+  /**
+   * Constructor
+   * Accepts a variable-length list of AutoCommand pointers to execute concurrently
+   * @param numCommands the number of commands to execute
+   * @param ... the commands to run
+   */
   ConcurrentCommand(int numCommands, ...);
+
+  /**
+   * Destructor
+   * Deallocates the AutoCommands stored in commands_
+   */
   virtual ~ConcurrentCommand();
+
+  /**
+   * Calls Initialize() on all the AutoCommands in commands_
+   */
   virtual void Initialize();
+
+  /**
+   * Calls Run() on all the unfinished AutoCommands in commands_
+   * Updates the bool of each CommandPair to true once the command is completed
+   * @return true if all of the commands are complete, else false
+   */
   virtual bool Run();
  private:
-  // vector of pairs of each command and its completion status
-  // these are initialized as false and updated to true as
-  // commands are completed
+  // CommandPair represents a command and its completion state.
+  // The completion state is initalized to false and is updated every time Run() is called.
   typedef std::pair<AutoCommand*, bool> CommandPair;
   std::vector<CommandPair> commands_;
 };
+
+#endif // CONCURRENT_COMMAND_H_
 
