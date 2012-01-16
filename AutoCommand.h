1b330633708d505151bec9a2a5d87f11d470cd1b
Eric Bakan <eric@ebakan.com>
2012-01-15 23:38:41 -0800

    Added include guard, updated comment styling

--- AutoCommand.h
+++ AutoCommand.h
@@ -1,28 +1,35 @@
 /**
-  * Represents a command to be executed in Autonomous Mode
-  * Is the base class for all other Autonomous Commands (driving, shooting, etc.)
-  * @author Eric Bakan
-  */
+ * @author Eric Bakan
+ *
+ * Represents a command to be executed in Autonomous Mode
+ * Is the base class for all other Autonomous Commands (driving, shooting, etc.)
+ */
+
+#ifndef AUTO_COMMAND_H_
+#define AUTO_COMMAND_H_
+
 class AutoCommand {
  public:
   /**
-    * Initialization method
-    * Is called the first time the AutoCommand is processed
-    * Used to set control loop goals, etc.
-    */
+   * Initialization method
+   * Is called the first time the AutoCommand is processed
+   * Used to set control loop goals, etc.
+   */
   virtual void Initialize();
 
   /**
-    * Run method
-    * Is called every time the autonomous loop iterates
-    * Used to update control loops, etc.
-    * @return true if the command is completed, otherwise false
-    */
+   * Run method
+   * Is called every time the autonomous loop iterates
+   * Used to update control loops, etc.
+   * @return true if the command is completed, otherwise false
+   */
   virtual bool Run() = 0;
 
   /**
-    * Destructor, doesn't do much...
-    */
+   * Destructor, doesn't do much...
+   */
   virtual ~AutoCommand();
 };
+
+#endif // AUTO_COMMAND_H_
 
