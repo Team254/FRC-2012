20b18b1e76fa61531364c0f496ea0fbe97749db8
Eric Bakan <eric@ebakan.com>
2012-01-18 18:40:23 -0800

    Commented Drive.h

--- src/subsystems/Drive.h
+++ src/subsystems/Drive.h
@@ -5,17 +5,50 @@
 
 #include "config/Constants.h"
 
+/**
+ * @author Eric Caldwell
+ * 
+ * Easy-access functions for drive functions: setting power, getting encoder values, etc.
+ */
 class Drive {
  public:
+  /**
+   * Constructor
+   * Accepts the Victors and Encoders to get and set values
+   */
   Drive(Victor* leftA, Victor* leftB, Victor* rightA, Victor* rightB);
+
+  /**
+   * Sets power to the left and right sides of the drivetrain
+   * @param left left power
+   * @param right right power
+   */
   void SetPower(double left, double right);
+
+  /**
+   * Sets the power to the left wheel
+   * Automatically caps the power to +1.0 or -1.0
+   * @param power the power to set
+   */
   void SetLeftDrivePower(double power);
+
+  /**
+   * Sets the power to the left wheel
+   * Automatically caps the power to +1.0 or -1.0
+   * @param power the power to set
+   */
   void SetRightDrivePower(double power);
+
  private:
+
+  // Victors
+
   Victor* leftDriveMotorA_;
   Victor* leftDriveMotorB_;
   Victor* rightDriveMotorA_;
   Victor* rightDriveMotorB_;
+
+  // Sensors
 
   Constants* constants;
 };
