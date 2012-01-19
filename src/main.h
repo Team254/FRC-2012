1feec12f96d85506783270c68140d167ce43024e
Eric Bakan <eric@ebakan.com>
2012-01-18 18:23:51 -0800

    Added comments to main.h

--- src/main.h
+++ src/main.h
@@ -5,34 +5,83 @@
 
 #include "subsystems/Drive.h"
 
+/**
+ * @author Eric Caldwell
+ * 
+ * The main robot class. Almost all of our logic outside of individual subsystems will be placed in here.
+ */
 class MainRobot : public IterativeRobot {
  public:
+  /**
+   * Constructor
+   * Initializes all motors, sensors, and control board inputs the program will use
+   * Also constructs support subsystem classes
+   */
   MainRobot();	
 	
+  /**
+   * Runs once when the robot enters Disabled mode
+   */
   virtual void DisabledInit();
+
+  /**
+   * Runs once when the robot enters Autonomous mode
+   */
   virtual void AutonomousInit();
+
+  /**
+   * Runs once when the robot enters Teleop mode
+   */
   virtual void TeleopInit();
 
+  /**
+   * Is called periodically as long as the robot is in Disabled mode
+   */
   virtual void DisabledPeriodic();
+
+  /**
+   * Is called periodically as long as the robot is in Autonomous mode
+   */
   virtual void AutonomousPeriodic();
+
+  /**
+   * Is called periodically as long as the robot is in Teleop mode
+   */
   virtual void TeleopPeriodic();
 
   // Helper functions
+
+  /**
+   * Implements a deadband on a joystick
+   * @param val the joystick value
+   * @param deadband the maximum value the deadband will return 0 for
+   * @return 0.0 if the absolute value of the joystick value is less than the deadband, else the joystick value
+   */
   double HandleDeadband(double val, double deadband);
 
  private:
 
+  // Constants
+
   Constants* constants_;
+
+  // Subsystems
 
   Drive* drivebase_;
 
+  // Joysticks
+
   Joystick* leftJoystick_;
   Joystick* rightJoystick_;
+
+  // Motors
 
   Victor* leftDriveMotorA_;
   Victor* leftDriveMotorB_;
   Victor* rightDriveMotorA_;
   Victor* rightDriveMotorB_;
+
+  // Sensors
 };
 
 // Start the actual program
