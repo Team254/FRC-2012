555ea0b3b0ff58656d814c06ba4c57e413cbf615
Richard Lin <richard.lin13@bcp.org>
2012-01-25 18:07:31 -0800

    Style fixes

--- src/auto/DriveCommand.cpp
+++ src/auto/DriveCommand.cpp
@@ -1,37 +1,37 @@
 #include "auto/DriveCommand.h"
 
 DriveCommand::DriveCommand(Drive* drive, double distance) {
-	drive_ = drive;
-	distanceGoal_ = distance;
-	//set constanst for tuning here
-	leftPid_ = new Pid(.1, 0, 0);
-	leftPid_->SetGoal(distanceGoal_);
-	rightPid_ = new Pid(.1, 0, 0);
-	rightPid_->SetGoal(distanceGoal_);
-	distanceGoal_ = distance;
+  drive_ = drive;
+  distanceGoal_ = distance;
+  // set constants for tuning here
+  leftPid_ = new Pid(.1, 0, 0);
+  leftPid_->SetGoal(distanceGoal_);
+  rightPid_ = new Pid(.1, 0, 0);
+  rightPid_->SetGoal(distanceGoal_);
+  distanceGoal_ = distance;
 }
 
 void DriveCommand::Initialize() {
-	drive_->ResetEncoders();
+  drive_->ResetEncoders();
 }
 
 bool DriveCommand::Run() {
-	double currLeftDist = drive_->GetLeftEncoderDistance();
-	double currRightDist = drive_->GetRightEncoderDistance();
-	//if goal reached, return true so current command can be popped off queue
-	if(currLeftDist == distanceGoal_ && currRightDist == distanceGoal_) {
-		return true;
-	}
-	//get PID feedback and send back to motors
-	double leftPwr = leftPid_->Update(currLeftDist);
-	double rightPwr = rightPid_->Update(currRightDist);
-	drive_->SetLinearPower(leftPwr, rightPwr);
-	//indicates that goal is not reached
-	return false;
+  double currLeftDist = drive_->GetLeftEncoderDistance();
+  double currRightDist = drive_->GetRightEncoderDistance();
+  // if goal reached, return true so current command can be popped off queue
+  if(currLeftDist == distanceGoal_ && currRightDist == distanceGoal_) {
+    return true;
+  }
+  // get PID feedback and send back to motors
+  double leftPwr = leftPid_->Update(currLeftDist);
+  double rightPwr = rightPid_->Update(currRightDist);
+  drive_->SetLinearPower(leftPwr, rightPwr);
+  // indicates that goal is not reached
+  return false;
 }
 
 DriveCommand::~DriveCommand() {
-	delete leftPid_;
-	delete rightPid_;
-	delete drive_;
+  delete leftPid_;
+  delete rightPid_;
+  delete drive_;
 }
