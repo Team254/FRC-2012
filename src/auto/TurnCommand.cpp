cd6cadafefeffcee7dfb548fa4c3a3315fa6ce39
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2012-02-14 22:25:39 -0800

    Added TurnCommand

---
+++ src/auto/TurnCommand.cpp
@@ -0,0 +1,29 @@
+#include "auto/TurnCommand.h"
+
+#include "subsystems/Drive.h"
+#include "subsystems/Pid.h"
+
+TurnCommand::TurnCommand(Drive* drive, double angle, double timeout) {
+  SetTimeout(timeout);
+  angle_ = angle;
+  drive_ = drive;
+  oldAngle_ = 9999;
+  turnPid_ = new Pid(0,0,0); // TODO add constants
+}
+
+void TurnCommand::Initialize() {
+  drive_->ResetGyro();
+}
+
+bool TurnCommand::Run() {
+  double curAngle = drive_->GetGyroAngle();
+  double power = turnPid_->Update(angle_, curAngle);
+  drive_->SetLinearPower(-power, power);
+
+  if (curAngle - angle_ < 1.0 && curAngle - oldAngle_ < .1) { // Make this better
+    return true;
+  }
+  oldAngle_ = curAngle;
+
+  return TimeoutExpired();
+}
---
+++ src/auto/TurnCommand.h
@@ -0,0 +1,35 @@
+#ifndef AUTO_TURN_CMD_H_
+#define AUTO_TURN_CMD_H_
+
+#include "auto/AutoCommand.h"
+
+class Drive;
+class Pid;
+
+class TurnCommand : public AutoCommand {
+ public:
+  /**
+   * Constructor. Takes a drive object and the angle to turn.
+   */
+  TurnCommand(Drive* drive, double angle, double timeout);
+
+  /**
+   * Initializes 
+   */
+  void Initialize();
+
+  /**
+   * Uses PID to turn to the wanted angle. Returns true when complete.
+   */
+  bool Run();
+
+  
+
+ private:
+  Drive* drive_;
+  Pid* turnPid_;
+  double angle_, oldAngle_;
+
+};
+
+#endif
