e62c83b062254a53b27f0c53a0e255e207b5c409
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2012-03-11 22:21:26 -0700

    Added Caldwell's bridge balls command

---
+++ src/auto/BridgeBallsCommand.cpp
@@ -0,0 +1,50 @@
+#include "auto/BridgeBallsCommand.h"
+#include "subsystems/Intake.h"
+#include "subsystems/Shooter.h"
+
+BridgeBallsCommand::BridgeBallsCommand(Intake* intake, Shooter* shooter, 
+                                      double timeout) {
+  SetTimeout(timeout);
+  intake_ = intake;
+  shooter_ = shooter;
+  state_ = 0;
+  initTime_ = 0;
+}
+
+void BridgeBallsCommand::Initialize() {
+  state_ = 0; 
+  initTime_ = timer_->Get();
+}
+
+bool BridgeBallsCommand::Run(){
+  switch(state_)
+  {
+  	case 0:
+  	  intake_->SetIntakePosition(Intake::INTAKE_DOWN);
+  	  state_ = 1;
+  	  break;
+  	case 1:
+  	  intake_->SetIntakePower(1);
+  	  if(timer_->Get() - initTime_ > .5) {
+  	    intake_->SetIntakePosition(Intake::INTAKE_FLOATING);
+  	    state_ = 2;
+  	  }
+  	  break;
+  	case 2:
+  	  intake_->SetIntakePower(1);
+  	  if(timer_->Get() - initTime_ > 2.5) {
+  	    intake_->SetIntakePosition(Intake::INTAKE_UP);
+  	    state_ = 3;
+  	    return true;
+  	  }
+  	  break;
+  	default:
+  	  printf("WTF am I doing in here");	
+  }
+  return TimeoutExpired();
+}
+
+BridgeBallsCommand ::~BridgeBallsCommand() {
+  delete intake_;
+  delete shooter_;
+}
