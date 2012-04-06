ad81fd2f613a4e7002f74b9def9601a416b44c9c
Eric Bakan <eric@ebakan.com>
2012-04-05 18:28:04 -0700

    Added OldDriveCommand

---
+++ src/auto/OldDriveCommand.cpp
@@ -0,0 +1,125 @@
+#include "auto/OldDriveCommand.h"
+
+#include "subsystems/Drive.h"
+#include "subsystems/Pid.h"
+#include "util/PidTuner.h"
+#include "config/Constants.h"
+#include "matlab/mat.h"
+#include "util/ContinuousAccelFilter.h"
+
+OldDriveCommand::OldDriveCommand(Drive* drive, double distance, double angle, bool usePizza, double timeout, double maxSpeed, double maxAcceleration, double maxAlpha, double maxOmega) {
+  SetTimeout(timeout);
+  drive_ = drive;
+  angleGoal_ = angle;
+  distanceGoal_ = distance;
+  usePizza_ = usePizza;
+  resetPizza_ =  usePizza; //(usePizza && drive->GetPizzaUp());
+  maxSpeed_ = maxSpeed;
+  maxAcceleration_ = maxAcceleration;
+  maxOmega_ = maxOmega;
+  maxAlpha_ = maxAlpha;
+
+  Constants* constants = Constants::GetInstance();
+  leftPid_ = new Pid(&constants->driveKP, &constants->driveKI, &constants->driveKD);
+  rightPid_ = new Pid(&constants->driveKP, &constants->driveKI, &constants->driveKD);
+  brakeTimer_ = new Timer();
+  ssc_ = new ss_controller(2, 2, 4, ss_controller::DRIVE);
+  y_ = init_matrix(2, 1);
+  r_ = init_matrix(4, 1);
+  straightFilter_ = new ContinuousAccelFilter();
+  turnFilter_ = new ContinuousAccelFilter();
+  
+  // Old
+  driveTimer_ = new Timer();
+  prevTime_ = 0;
+  prevLeftDist_ = 0;
+  prevRightDist_ = 0;
+  startingAngle_ = 0;
+}
+
+void OldDriveCommand::Initialize() {
+  AutoCommand::Initialize();
+  drive_->ResetEncoders();
+  drive_->SetPizzaWheelDown(usePizza_);
+  brakeTimer_->Reset();
+  curA_ = 0.0;
+  curV_ = 0.0;
+  curX_ = 0.0;
+  curJeez_ = 0.0;
+  curWubl_ = 0.0;
+  curThet_ = 0.0;
+  turnOffset_ = 0.0;
+  sumStoppedError_ = 0.0;
+  flash_matrix(y_, 0.0, 0.0);
+  flash_matrix(r_, 0.0, 0.0, 0.0, 0.0);
+  ssc_->reset();
+  delete straightFilter_;
+  delete turnFilter_;
+  straightFilter_ = new ContinuousAccelFilter();
+  turnFilter_ = new ContinuousAccelFilter();
+  
+  // Old
+  prevTime_ = 0;
+  prevLeftDist_ = drive_->GetLeftEncoderDistance();;
+  prevRightDist_ = drive_->GetRightEncoderDistance();;
+  driveTimer_->Start();
+  driveTimer_->Reset();
+  startingAngle_ = drive_->GetGyroAngle();
+}
+
+bool OldDriveCommand::Run() {
+  
+  if (TimeoutExpired()) {
+    drive_->SetLinearPower(0, 0);
+    return true;
+    }
+    drive_->SetHighGear(true);
+    double currLeftDist = drive_->GetLeftEncoderDistance();
+    double currRightDist = drive_->GetRightEncoderDistance();
+    double currTime = driveTimer_->Get();
+    double lVel = (currLeftDist - prevLeftDist_)/(currTime - prevTime_);
+    double rVel = (currRightDist - prevRightDist_)/(currTime - prevTime_);
+    prevTime_ = currTime;
+    prevLeftDist_ = currLeftDist;
+    prevRightDist_ = currRightDist;
+    
+
+    // Get PID feedback and send back to the motors.
+    double leftPIDOutput = PwmLimit(leftPid_->Update(distanceGoal_, currLeftDist));
+    double rightPIDOutput = PwmLimit(rightPid_->Update(distanceGoal_, currRightDist));
+    double angleDiff = drive_->GetGyroAngle() - startingAngle_;
+    double straightGain = angleDiff * Constants::GetInstance()->straightDriveGain;
+    double leftPwr = leftPIDOutput - straightGain;
+    double rightPwr = rightPIDOutput + straightGain;
+    
+    leftPwr = (leftPwr < -maxSpeed_) ?  -maxSpeed_: (leftPwr > maxSpeed_) ? maxSpeed_ : leftPwr;
+    rightPwr = (rightPwr < -maxSpeed_) ?  -maxSpeed_ : (rightPwr > maxSpeed_) ? maxSpeed_ : rightPwr;
+    
+    leftPwr -= straightGain;
+    rightPwr += straightGain;
+    //PidTuner::PushData(currLeftDist, distanceGoal_, 0.0);    
+    drive_->SetLinearPower(leftPwr, rightPwr);
+    
+    if (fabs(currLeftDist - distanceGoal_ ) < 2 || fabs(currRightDist- distanceGoal_) < 2) {
+      //if (coast_) {
+      if (false) {
+        drive_->SetLinearPower(0,0);
+        return true;
+       }
+      if (fabs(lVel) < 6 && fabs(rVel) < 6) {
+        brakeTimer_->Start();
+      }
+
+      }
+    
+    if (brakeTimer_->Get() > .2) {
+      drive_->SetPizzaWheelDown(resetPizza_);
+      drive_->SetLinearPower(0,0);
+      return true;
+    }
+    // Indicate that the goal has not yet been reached.
+    return false;
+}
+
+OldDriveCommand::~OldDriveCommand() {
+}
---
+++ src/auto/OldDriveCommand.h
@@ -0,0 +1,83 @@
+#ifndef AUTO_OLD_DRIVE_COMMAND_H_
+#define AUTO_OLD_DRIVE_COMMAND_H_
+
+#include "auto/AutoCommand.h"
+#include "util/MovingAverageFilter.h"
+
+class Drive;
+class Pid;
+class ss_controller;
+class AccelFilterBase;
+/**
+ * @author Bhargava Manja
+ *
+ * This class encompasses all drive functionality in autonomous mode and includes PID error handling for
+ * distance.
+ */
+class OldDriveCommand : public AutoCommand {
+ public:
+  /**
+   * Constructor. Accepts drivetrain pointer and goal in terms of distance in inches
+   */
+	// coast is defunct - take it out when we refactor
+  OldDriveCommand(Drive* drive, double distance, double angle, bool usePizza, double timeout, double maxSpeed=240.0, double maxAcceleration=120.0, double maxAlpha=10.0, double maxOmega=10.0);
+
+  /**
+   * Clears encoders and gyro
+   */
+  void Initialize();
+
+  /**
+   * Uses PID update values to set motors and reach distance goal with minimum error
+   */
+  bool Run();
+
+  /**
+   * Destructor
+   */
+ ~OldDriveCommand();
+ private:
+  // Drivebase object for motor control and encoder reading functionality
+  Drive* drive_;
+  
+  //Timer for velocity calcs
+  Timer* brakeTimer_;
+
+  // Distance goal, more goals can be added (angle, velocity, etc.)
+  double distanceGoal_;
+  double angleGoal_;
+
+  // Should pizza wheels be down?
+  bool usePizza_;
+  bool resetPizza_;
+  double maxSpeed_;
+  double maxAcceleration_;
+  double maxAlpha_;
+  double maxOmega_;
+  double turnOffset_;
+  double sumStoppedError_;
+  
+  // State space stuff
+  double curA_;
+  double curV_;
+  double curX_;
+  double curJeez_; // Jesusfish
+  double curWubl_; // Wubbleu
+  double curThet_; // Theta
+  struct matrix *y_;
+  struct matrix *r_;
+  ss_controller *ssc_;
+  AccelFilterBase* straightFilter_;
+  AccelFilterBase* turnFilter_;
+  
+  // Old
+  Pid* leftPid_;
+  Pid* rightPid_;
+  Timer* driveTimer_;
+  double prevTime_;
+  double prevLeftDist_;
+  double prevRightDist_;
+  double startingAngle_;
+};
+
+#endif  // AUTO_OLD_DRIVE_COMMAND_H_
