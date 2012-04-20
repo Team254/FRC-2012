aef45acd8637b463545d55cbe8e137aab9fbe8d5
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2012-04-20 10:46:44 -0700

    Add SetIntakePositionCommand

---
+++ src/auto/SetIntakePositionCommand.h
@@ -0,0 +1,27 @@
+#ifndef AUTO_INTAKE_POS_COMMAND_H_
+#define AUTO_INTAKE_POS_COMMAND_H_
+
+#include "auto/AutoCommand.h"
+#include "subsystems/Intake.h"
+
+class SetIntakePositionCommand : public AutoCommand {
+
+ public:
+  
+  SetIntakePositionCommand(Intake* intake, Intake::IntakePositions pos);
+  
+  void Initialize();
+  
+  bool Run();
+  
+  ~SetIntakePositionCommand();
+  
+ private:
+  
+  Intake* intake_;
+  Intake::IntakePositions pos_;
+  
+};
+
+#endif // AUTO_INTAKE_POS_COMMAND_H_
+
